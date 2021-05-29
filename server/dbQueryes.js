57c104cc2559fd379c7c0b09157d1127ba494e7e
greyewi1 <greyewi@gmail.com>
2021-05-29 18:32:57 +0500

    add project CRUD

--- server/dbQueryes.js
+++ server/dbQueryes.js
@@ -3,11 +3,21 @@ const sqlProjects = {
 }
 
 const sqlInsertProject = {
-  text: 'INSERT INTO projectdeskdb.projects (name, code) VALUES ($1, $2)'
+  text: `INSERT INTO projectdeskdb.projects (name, code) VALUES ($1, $2) RETURNING id, name, code`
+}
+
+const sqlDeleteProject = {
+  text: `DELETE FROM projectdeskdb.projects WHERE id = $1 RETURNING id, name, code`
+}
+
+const sqlUpdateProject = {
+  text: `UPDATE projectdeskdb.projects SET name = $2, code = $3 WHERE id = $1 RETURNING id, name, code`
 }
 
 
 module.exports = {
   sqlProjects,
-  sqlInsertProject
+  sqlInsertProject,
+  sqlDeleteProject,
+  sqlUpdateProject
 }
--- server/index.js
+++ server/index.js
@@ -2,13 +2,14 @@ const express = require('express'), bodyParser = require('body-parser'), cors = 
 const app = express()
 const {origin, port} = require('./constants')
 const connect = require('./database')
-const {sqlProjects} = require('./dbQueryes')
+const {sqlProjects, sqlInsertProject, sqlDeleteProject, sqlUpdateProject} = require('./dbQueryes')
 
 
 app.use(express.urlencoded({extended: true}))
 app.use(bodyParser.json())
 app.use(cors({origin : origin}))
 app.use(express.json())
+
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Credentials", 'true')
   res.header("Access-Control-Allow-Origin", origin)
@@ -19,6 +20,24 @@ app.use((req, res, next) => {
 
 app.get('/api/v1/projects/', async (req, res) => {
   const {rows} = await connect.query(sqlProjects)
+  res.status(200).send(rows)
+})
+
+app.post('/api/v1/projects/', async (req, res) => {
+  const {body: {name, code}} = req
+  const {rows} = await connect.query(sqlInsertProject, [name, code])
+  res.status(200).send(rows)
+})
+
+app.delete('/api/v1/projects/', async (req, res) => {
+  const {body: {id}} = req
+  const {rows} = await connect.query(sqlDeleteProject, [id])
+  res.status(200).send(rows)
+})
+
+app.put('/api/v1/projects/', async (req, res) => {
+  const {body: {id, name, code}} = req
+  const {rows} = await connect.query(sqlUpdateProject, [id, name, code])
   res.status(200).send(rows)
 })
 
