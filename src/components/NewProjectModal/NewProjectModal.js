edb9a1f28b087adc80c8a6468b0650dd18c0a028
greyewi1 <greyewi@gmail.com>
2021-06-20 18:56:40 +0500

    add formik

--- src/components/NewProjectModal/NewProjectModal.js
+++ src/components/NewProjectModal/NewProjectModal.js
@@ -1,16 +1,13 @@
-import React, { useState } from 'react';
+import React  from 'react';
 import Modal from '../Modal';
 import './newProjectModal.css';
+import {Field, Form, Formik} from 'formik'
 
 const NewProjectModal = (props) => {
     const { open, setOpen, projectModalAction, title, projectItem = {} } = props;
-    const { name = '', code = '', id = '' } = projectItem;
-    const [projectName, setProjectName] = useState(name);
-    const [projectCode, setProjectCode] = useState(code);
 
-    const handleSubmit = (evt) => {
-        evt.preventDefault();
-        projectModalAction({ id, projectName, projectCode });
+    const handleSubmit = (values) => {
+        projectModalAction(values);
     }
 
     return (
@@ -20,28 +17,38 @@ const NewProjectModal = (props) => {
                     title={title}
                     onClose={() => setOpen(false)}
                 >
-                    <form onSubmit={handleSubmit}>
-                        <p>Name</p>
-                        <input
-                            className='inputForm'
-                            type='text'
-                            name='projectName'
-                            value={projectName}
-                            onChange={e => setProjectName(e.target.value)}
-                        />
-                        <p>Code</p>
-                        <input
-                            className='inputForm'
-                            type='text'
-                            name='projectCode'
-                            value={projectCode}
-                            onChange={e => setProjectCode(e.target.value)}
-                        />
-                        <input
-                            type='submit'
-                            className='submitBtn'
-                        />
-                    </form>
+                    <Formik
+                      initialValues={projectItem}
+                      validate={(name, code) => {
+                          const errors = {}
+                          if(!name) {
+                              errors.name = "Name is required"
+                          }
+                          if(!code) {
+                              errors.code = "Code is required"
+                          }
+                          return errors
+                      }}
+                      onSubmit={handleSubmit}
+                    >
+                        <Form>
+                            <p>Name</p>
+                            <Field
+                              className='inputForm'
+                              name='name'
+                            />
+                            <p>Code</p>
+                            <Field
+                              className='inputForm'
+                              name='code'
+                            />
+                            <button
+                              type='submit'
+                              className='submitBtn'
+                            >Add</button>
+                        </Form>
+                    </Formik>
+
                 </Modal>}
         </div>
     )
